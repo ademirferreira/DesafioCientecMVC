755e292b3117c7e9e16a1209254df95b07298cd4
Ademir Ferreira <whoisademir@gmail.com>
2022-04-06 14:22:43 -0300

    Adicionados as interfaces e repositórios

---
+++ DesafioCientec.Business/Interfaces/IFundacaoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DesafioCientec.Business.Models;
+
+namespace DesafioCientec.Business.Interfaces
+{
+    public interface IFundacaoRepository : IRepository<Fundacao>
+    {
+        Task<Fundacao> BuscarPorDocumento(string documento);
+    }
+}
---
+++ DesafioCientec.Data/Context/FundacaoContext.cs
@@ -0,0 +1,40 @@
+﻿using DesafioCientec.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioCientec.Data.Context
+{
+    public class FundacaoContext : DbContext
+    {
+        public FundacaoContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Fundacao> Fundacoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fundacao>().HasKey(f => f.Id);
+            modelBuilder.Entity<Fundacao>()
+                .Property(f => f.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(400)");
+
+            modelBuilder.Entity<Fundacao>()
+                .Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fundacao>()
+                .Property(p => p.Email)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fundacao>()
+                .Property(p => p.Telefone)
+                .IsRequired()
+                .HasColumnType("varchar(15)");
+
+            modelBuilder.Entity<Fundacao>()
+                .Property(f => f.InstituicaoApoiada)
+                .HasColumnType("varchar(400)");
+        }
+    }
+}
