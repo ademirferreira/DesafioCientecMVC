1c898443effecc5f52ce9c34069e0653bac666cd
Ademir Ferreira <whoisademir@gmail.com>
2022-04-06 14:22:19 -0300

    Adicionados os models

---
+++ DesafioCientec.Business/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesafioCientec.Business.Models
+{
+    public class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
